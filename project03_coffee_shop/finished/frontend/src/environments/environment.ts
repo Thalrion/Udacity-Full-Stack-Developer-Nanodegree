9f1e1b9038115aa0314c5e6d13a35750256d723b
Matthew Ludwig <matthew.ludwig@web.de>
2019-12-16 12:53:21 +0100

    change auth0 domain prefix

--- project03_coffee_shop/finished/frontend/src/environments/environment.ts
+++ project03_coffee_shop/finished/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'fsnd-matthew.eu.auth0.com', // the auth0 domain prefix
+    url: 'fsnd-matthew.eu', // the auth0 domain prefix
     audience: 'Coffee', // the audience set for the auth0 app
     clientId: 'OIo6L9Fo3gwtnnNi07V2Aj1IZ6WeZPst', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
