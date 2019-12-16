69e6e9d39664fe04b898ab780b83d2c973fd340d
Matthew Ludwig <matthew.ludwig@web.de>
2019-12-16 12:50:22 +0100

    add Enviornment Variables to Ionic configuration file

--- project03_coffee_shop/finished/frontend/src/environments/environment.ts
+++ project03_coffee_shop/finished/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'fsnd-matthew.eu.auth0.com', // the auth0 domain prefix
+    audience: 'Coffee', // the audience set for the auth0 app
+    clientId: 'OIo6L9Fo3gwtnnNi07V2Aj1IZ6WeZPst', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
